f48c27c636dfd2b59357e397f48dad84e6fe3c1c
Wesley Xavier <wesleybruce@gmail.com>
2020-07-19 00:31:56 -0300

    refatorado

--- src/main/java/com/traineeprimeiro/domain/entities/EntityBase.java
+++ src/main/java/com/traineeprimeiro/domain/entities/EntityBase.java
@@ -1,18 +1,16 @@
 package com.traineeprimeiro.domain.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @Setter
-@Entity
+@MappedSuperclass
 public abstract class EntityBase {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
